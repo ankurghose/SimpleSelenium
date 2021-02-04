89d5a266972c4280b23c9c9f297b3848d084cec5
ankur <ankur@192.168.1.8>
2021-02-04 09:58:31 -0500

    "I didnt review it push lol"

--- SimpleSelenium /src/test/java/com/common/Children.java
+++ SimpleSelenium /src/test/java/com/common/Children.java
@@ -7,7 +7,7 @@ import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class Base {
+public class Children {
 	
 	public WebDriver driver;
 	
